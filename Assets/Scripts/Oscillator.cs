c470a69912f09b70322d279f86386acc5d5cfac9
Matěj Beran <matej.beran96@gmail.com>
2020-02-19 21:10:48 +0100

    Protect against NaN.

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -8,23 +8,27 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movement;
     [SerializeField] float period;
 
-    private Vector3 startingPosition;
+    Vector3 startingPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
-        this.startingPosition = transform.position;
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // todo: protect against period being 0
+        DoOscillation();
+    }
+
+    void DoOscillation()
+    {
+        if (period <= float.Epsilon) { return; }
+
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2f;
         float sine = Mathf.Sin(cycles * tau);
 
         Vector3 offset = movement * (sine / 2f + 0.5f);
-        transform.position = this.startingPosition + offset;
+        transform.position = startingPosition + offset;
     }
 }
